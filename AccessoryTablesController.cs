f23006e156d66a4e277d019bd14c49b8c8fdc25c
Csar9000 <72266005+Csar9000@users.noreply.github.com>
2022-05-30 23:12:29 +0800

    Доработана сборка комплектации компьютера: комплектующие можно удалять; Добавлено суммирование цен комлпектующих;

--- AccessoryTablesController.cs
+++ AccessoryTablesController.cs
@@ -7,6 +7,7 @@ namespace ComputerAssemblyShop
 {
     public class AccessoryTablesController : Controller
     {
+
         private ComputerAssemblyEntities db = new ComputerAssemblyEntities();
 
         // GET: AccessoryTables
@@ -23,58 +24,174 @@ namespace ComputerAssemblyShop
 
         public ActionResult addComplect(int? id)
         {
+            if (Session["ResultSum"] == null)
+            {
+                Session["ResultSum"] = 0;
+            }
+
             AccessoryTable accessoryTable = db.AccessoryTable.Find(id);
             switch (accessoryTable.type)
             {
                 case "vid":
                     {
+                        Session["vidId"] = accessoryTable.id;
                         Session["VideoCardName"] = accessoryTable.name;
+                        Session["VideoCardPrice"] = accessoryTable.price;
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) + accessoryTable.price;
                         break;
                     }
                 case "proc":
                     {
+                        Session["procId"] = accessoryTable.id;
                         Session["ProcessorName"] = accessoryTable.name;
+                        Session["ProcPrice"] = accessoryTable.price;
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) + accessoryTable.price;
                         break;
                     }
                 case "disc":
                     {
+                        Session["discId"] = accessoryTable.id;
                         Session["DiskName"] = accessoryTable.name;
+                        Session["DiskPrice"] = accessoryTable.price;
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) + accessoryTable.price;
                         break;
                     }
                 case "ozu":
                     {
+                        Session["ozuId"] = accessoryTable.id;
                         Session["OZUName"] = accessoryTable.name;
+                        Session["OZUPrice"] = accessoryTable.price;
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) + accessoryTable.price;
                         break;
                     }
                 case "kuler":
                     {
+                        Session["kulerId"] = accessoryTable.id;
                         Session["KulerName"] = accessoryTable.name;
+                        Session["KulerPrice"] = accessoryTable.price;
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) + accessoryTable.price;
                         break;
                     }
                 case "korpus":
                     {
+                        Session["korpusId"] = accessoryTable.id;
                         Session["KorpusName"] = accessoryTable.name;
+                        Session["KorpusPrice"] = accessoryTable.price;
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) + accessoryTable.price;
                         break;
                     }
                 case "monitor":
                     {
+                        Session["monitorId"] = accessoryTable.id;
                         Session["MonitorName"] = accessoryTable.name;
+                        Session["MonitorPrice"] = accessoryTable.price;
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) + accessoryTable.price;
                         break;
                     }
                 case "mouse":
                     {
+                        Session["mouseId"] = accessoryTable.id;
                         Session["MouseName"] = accessoryTable.name;
+                        Session["MousePrice"] = accessoryTable.price;
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) + accessoryTable.price;
                         break;
                     }
                 case "keyboard":
                     {
+                        Session["keyboardId"] = accessoryTable.id;
                         Session["KeyboardName"] = accessoryTable.name;
+                        Session["KeyboardPrice"] = accessoryTable.price;
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) + accessoryTable.price;
                         break;
                     }
             }
 
             return RedirectToAction("Complectation");
         }
+
+
+        public ActionResult deleteComplect(int? id)
+        {
+            AccessoryTable accessoryTable = db.AccessoryTable.Find(id);
+            switch (accessoryTable.type)
+            {
+                case "vid":
+                    {
+                        Session["vidId"] = "";
+                        Session["VideoCardName"] = "";
+                        Session["VideoCardPrice"] = "";
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) - accessoryTable.price;
+                        break;
+                    }
+                case "proc":
+                    {
+                        Session["procId"] = "";
+                        Session["ProcessorName"] = "";
+                        Session["ProcPrice"] = "";
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) - accessoryTable.price;
+                        break;
+                    }
+                case "disc":
+                    {
+                        Session["discId"] = "";
+                        Session["DiskName"] = "";
+                        Session["DiskPrice"] = "";
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) - accessoryTable.price;
+                        break;
+                    }
+                case "ozu":
+                    {
+                        Session["ozuId"] = "";
+                        Session["OZUName"] = "";
+                        Session["OZUPrice"] = "";
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) - accessoryTable.price;
+                        break;
+                    }
+                case "kuler":
+                    {
+                        Session["kulerId"] = "";
+                        Session["KulerName"] = "";
+                        Session["KulerPrice"] = "";
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) - accessoryTable.price;
+                        break;
+                    }
+                case "korpus":
+                    {
+                        Session["korpusId"] = "";
+                        Session["KorpusName"] = "";
+                        Session["KorpusPrice"] = "";
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) - accessoryTable.price;
+                        break;
+                    }
+                case "monitor":
+                    {
+                        Session["monitorId"] = "";
+                        Session["MonitorName"] = "";
+                        Session["MonitorPrice"] = "";
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) - accessoryTable.price;
+                        break;
+                    }
+                case "mouse":
+                    {
+                        Session["mouseId"] = "";
+                        Session["MouseName"] = "";
+                        Session["MousePrice"] = "";
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) - accessoryTable.price;
+                        break;
+                    }
+                case "keyboard":
+                    {
+                        Session["keyboardId"] = "";
+                        Session["KeyboardName"] = "";
+                        Session["KeyboardPrice"] = "";
+                        Session["ResultSum"] = int.Parse(Session["ResultSum"].ToString()) - accessoryTable.price;
+                        break;
+                    }
+            }
+
+            return RedirectToAction("Complectation");
+        }
+
 
 
         public ActionResult About()
@@ -95,16 +212,10 @@ namespace ComputerAssemblyShop
         // GET: AccessoryTables/Details/5
         public ActionResult Details(int? id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
             AccessoryTable accessoryTable = db.AccessoryTable.Find(id);
-            if (accessoryTable == null)
-            {
-                return HttpNotFound();
-            }
-            return View(accessoryTable);
+            if (accessoryTable != null)
+                return PartialView(accessoryTable);
+            return HttpNotFound();
         }
 
         // GET: AccessoryTables/Create
