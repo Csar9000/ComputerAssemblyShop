fdaca1bad4add40d5c18d931f4cddd7145109840
Csar9000 <72266005+Csar9000@users.noreply.github.com>
2022-06-02 22:11:10 +0800

    Поправлена табличка отображения заказов

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -5,7 +5,6 @@ namespace ComputerAssemblyShop
 {
     public class BundleConfig
     {
-        // Дополнительные сведения об объединении см. на странице https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
